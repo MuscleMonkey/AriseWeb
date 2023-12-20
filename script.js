85dbd8a7600958917aa53e50685bf49c043b8b8e
ronald curzon <curzonronald@gmail.com>
2023-12-20 21:36:12 +0800

    late commit

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-for (let i = 0; i < 5; i++) {
-    let para = document.createElement("p");
-    para.setAttribute("id", "item-para");
-    para.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.Non diam phasellus vestibulum lorem sed. Volutpat diam utvenenatis tellus in metus.';
-
-    let h3 = document.createElement("h3");
-    h3.setAttribute("id","item-title");
-    h3.textContent = "Title";
-
-    let title_paragraph = document.createElement("section");
-    title_paragraph.setAttribute("id", "title-paragraph");
-    title_paragraph.appendChild(h3);
-    title_paragraph.appendChild(para);
-
-    let img = document.createElement("img");
-    
-    let item = document.createElement("article");
-    item.setAttribute("class", "item");
-    item.setAttribute("id","item");
-    item.appendChild(img)
-    item.appendChild(title_paragraph);
-
-    let hr = document.createElement("hr");
-    let main = document.getElementById("main");
-    main.appendChild(item);
-    main.appendChild(hr);
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
+
+delay(3000).then(() => console.log('runs after 3 seconds'))
